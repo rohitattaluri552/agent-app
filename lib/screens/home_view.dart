bd7bba823e282cd4d4d5879a898e3a97480ba9a0
Rohit Attaluri <59006955+rohitattaluri@users.noreply.github.com>
2020-06-18 16:52:13 +0530

    Updating home view with sliver app bar

--- lib/screens/home_view.dart
+++ lib/screens/home_view.dart
@@ -15,19 +15,48 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.only(top: 32.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            welcomeNote(textColor),
-            upcomingEvents(textColor),
-            Container (
-              alignment: Alignment.topLeft,
-              margin: EdgeInsets.only(bottom: 8.0,top: 16.0, left: 16.0),
-              child: Text('updates'.toUpperCase(), style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.w400, color: textColor)),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext bc, bool innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              leading: Container(),
+              backgroundColor: Colors.transparent,
+              expandedHeight: 385.0,
+              flexibleSpace: FlexibleSpaceBar(
+                collapseMode: CollapseMode.pin,
+                background: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 32.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          welcomeNote(textColor),
+                          upcomingEvents(textColor),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                  ],
+                ),
+              ),
             ),
-            Expanded(child: Activities(),),
+          ];
+        },
+        body: Column(
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.only(bottom: 8.0, top: 16.0, left: 16.0),
+              child: Text('updates'.toUpperCase(),
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w400,
+                      color: textColor)),
+            ),
+            Expanded(
+              child: Activities(),
+            ),
           ],
         ),
       ),
@@ -37,7 +66,14 @@ class _HomeViewState extends State<HomeView> {
   Widget welcomeNote(textColor) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-      child: Text('Hi, Rohit!', style: TextStyle(fontSize: 24.0,color: textColor,fontWeight: FontWeight.w400,),),
+      child: Text(
+        'Hi, Rohit!',
+        style: TextStyle(
+          fontSize: 24.0,
+          color: textColor,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
     );
   }
 
@@ -45,32 +81,49 @@ class _HomeViewState extends State<HomeView> {
     final double eventTileHeight = 240;
 
     final eventDetails = [
-      {'title': 'EiffelTower', 'imageUrl': 'https://bit.ly/2ZnjgOa', 'suburb': 'Paris', 'eventTime': 'WED 22 JUL - 9:40 AM'},
-      {'title': 'EiffelTower', 'imageUrl': 'https://ofi-documents-dev.s3-ap-southeast-2.amazonaws.com/4.jpg', 'suburb': 'Paris', 'eventTime': 'FRI 16 AUG - 15:30 AM'},
-      {'title': 'EiffelTower', 'imageUrl': 'https://bit.ly/3bM2M4Q', 'suburb': 'Paris', 'eventTime': 'WED 20 JUNE - 8:00 AM'},
+      {
+        'title': 'EiffelTower',
+        'imageUrl': 'https://bit.ly/2ZnjgOa',
+        'suburb': 'Paris',
+        'eventTime': 'WED 22 JUL - 9:40 AM'
+      },
+      {
+        'title': 'EiffelTower',
+        'imageUrl':
+            'https://ofi-documents-dev.s3-ap-southeast-2.amazonaws.com/4.jpg',
+        'suburb': 'Paris',
+        'eventTime': 'FRI 16 AUG - 15:30 AM'
+      },
+      {
+        'title': 'EiffelTower',
+        'imageUrl': 'https://bit.ly/3bM2M4Q',
+        'suburb': 'Paris',
+        'eventTime': 'WED 20 JUNE - 8:00 AM'
+      },
     ];
 
-    final events = eventDetails.map((event) => EventTile(event: event)).toList();
+    final events =
+        eventDetails.map((event) => EventTile(event: event)).toList();
 
-    return Column(
-      children:[
-        Container (
-          alignment: Alignment.topLeft,
-          margin: EdgeInsets.only(bottom: 16.0, left: 16.0),
-          child: Text('upcoming'.toUpperCase(), style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.w400, color: textColor)),
-        ),
-        Container(
-          height: eventTileHeight,
-          margin: EdgeInsets.only(right: 8.0),
-          child: PageView (
-            controller: PageController(
-              initialPage: 1,
-              viewportFraction: .95,
-            ),
-            children: events,
+    return Column(children: [
+      Container(
+        alignment: Alignment.topLeft,
+        margin: EdgeInsets.only(bottom: 16.0, left: 16.0),
+        child: Text('upcoming'.toUpperCase(),
+            style: TextStyle(
+                fontSize: 18.0, fontWeight: FontWeight.w400, color: textColor)),
+      ),
+      Container(
+        height: eventTileHeight,
+        margin: EdgeInsets.only(right: 8.0),
+        child: PageView(
+          controller: PageController(
+            initialPage: 1,
+            viewportFraction: .95,
           ),
+          children: events,
         ),
-      ]
-    );
+      ),
+    ]);
   }
 }
